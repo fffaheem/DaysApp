0f3c9615334144dadae53245211a6d2d71ab22e6
fffaheem <mohdfaheemahmad5@gmail.com>
2026-07-18 10:29:52 +0530

    - fixed sidebar bug (absolute to fixed) - added animation - touch outside to close the sidebar

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
     homeBtn: document.getElementById("home-btn"),
     sidebarOut: document.querySelector(".sidebar-out"),
     sidebarCloseBtn: document.getElementById("sidebar-close-btn"),
+    sidebar: document.querySelector(".sidebar"),
   }
 
 
@@ -13,15 +14,22 @@
   elements.homeBtn.addEventListener("click", (e) => {
     elements.sidebarOut.classList.add("sidebar-active");
     elements.body.classList.add("modal-active");
+    elements.sidebar.classList.add("sidebar-slide");
   })
 
   elements.sidebarCloseBtn.addEventListener("click", (e) => {
+    elements.sidebar.classList.remove("sidebar-slide");
     elements.sidebarOut.classList.remove("sidebar-active");
     elements.body.classList.remove("modal-active");
   })
 
-  
-
-
+  elements.sidebarOut.addEventListener("click", (e) => {
+    if (e.target === elements.sidebarOut) {
+      elements.sidebar.classList.remove("sidebar-slide");
+      elements.sidebarOut.classList.remove("sidebar-active");
+      elements.body.classList.remove("modal-active");
+    }
+    // if(e.target.clase)
+  })
   
 })();
